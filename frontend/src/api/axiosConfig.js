ac992b8911c6d08d02d56e6a94abd43633851484
Mohit Kumar 1004 <mohitkr741004@gmail.com>
2024-03-03 18:57:05 +0530

    Frontend Deployment commit 1

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'http://localhost:8080/',
+    baseURL: 'https://movies-sack-1.onrender.com/',
     headers: {"ngrok-skip-browser-warning": "true"}
 })
